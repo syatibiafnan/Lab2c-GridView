a6662e91378c9aec69d762f5b7b931dd1a9bbf3d
syatibiafnan <syatibiafnan@gmail.com>
2021-11-26 22:28:22 +0800

    Lab 2c - Grid View

---
+++ app/src/main/java/com/example/lab2c_gridview/FullImageActivity.java
@@ -0,0 +1,29 @@
+package com.example.lab2c_gridview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class FullImageActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstaceSate){
+        super.onCreate(savedInstaceSate);
+        setContentView(R.layout.full_image);
+
+        Intent i = getIntent();
+
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+        ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
+        imageView.setImageResource(imageAdapter.thumbImages[position]);
+
+        Toast.makeText(this,"Full view image " + position, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+}
